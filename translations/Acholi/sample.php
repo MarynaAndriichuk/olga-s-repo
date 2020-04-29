f106eb352f481bd90c08e9e219fb558dcfce89bf
MarynaAndriichuk <44168624+MarynaAndriichuk@users.noreply.github.com>
2020-04-29 21:05:56 +0300

    New translations sample.php (Acholi)

---
+++ translations/Acholi/sample.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+
+// You can also use comments to leave out parts of a code line
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+
+<?php
+$color = "crwdns803612:0crwdne803612:0";
+?>
+
+<?php
+/* en.php - english language file */
+$messages['smth to translate'] = 'crwdns803614:0crwdne803614:0';
+$messages['hello'] = 'crwdns803616:0crwdne803616:0';
+?>
+
+</body>
+</html>
