b9ca8bf5da3a2e9d52cde0ea1c2d0708a28bf87e
MarynaAndriichuk <44168624+MarynaAndriichuk@users.noreply.github.com>
2020-05-08 04:39:53 +0300

    New translations sample.php (Romanian)

--- localized_files/%%android-code/sample.php
+++ localized_files/%%android-code/sample.php
@@ -19,13 +19,13 @@ echo $x;
 ?>
 
 <?php
-$color = "crwdns803582:0crwdne803582:0";
+$color = "green";
 ?>
 
 <?php
 /* en.php - english language file */
-$messages['smth to translate'] = 'crwdns803584:0crwdne803584:0';
-$messages['hello'] = 'crwdns803586:0crwdne803586:0';
+$messages['smth to translate'] = 'Text for translation';
+$messages['hello'] = 'Hello';
 ?>
 
 </body>
